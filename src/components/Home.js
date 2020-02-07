392bc26735e88a43560d176ab7fc2bd4d8c9ff2e
Diego Mello <diegoasmello@gmail.com>
2020-02-07 00:03:41 -0300

    wip: create RepositoriesList and working in layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,45 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useCallback } from "react";
+import { useSessionStorage } from "react-use";
+import RepositoriesList from "./RepositoriesList";
 import "../scss/Home.scss";
 
-const API_URL = "https://api.github.com/users/diegoasmello/repos";
+//const API_URL = 'https://api.github.com/users/diegoasmello/repos';
+const API_URL = "https://api.github.com/search/repositories";
 
 export default function Home() {
-  const [repositories, setRepositores] = useState([]);
+  const [value, setValue] = useSessionStorage("value", "");
+  const [repositories, setRepositores] = useSessionStorage("repositories", []);
 
-  const fetchData = useCallback(async () => {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    console.log(data);
-    setRepositores(data);
-  }, []);
+  const fetchData = useCallback(
+    async (value) => {
+      const response = await fetch(API_URL + "?q=" + value);
+      const data = await response.json();
+      setRepositores(data);
+    },
+    [setRepositores]
+  );
+
+  async function searchRepositores() {
+    if (value) {
+      fetchData(value);
+    } else {
+      alert("Você deve digitar algo!");
+    }
+  }
+
+  function enter(evt) {
+    if (evt.key === "Enter") searchRepositores();
+  }
+
+  /* useEffect(() => {
+    if (!value) {
+      setRepositores([]);
+    }
+  }, [value]) */
 
   useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    if (value) fetchData(value);
+  }, [fetchData, value]);
 
   return (
     <div className="home">
@@ -25,9 +48,15 @@ export default function Home() {
           <div className="col-lg-8 offset-lg-2 col-md-12 offset-md-0">
             <main className="content">
               <div className="input-container">
-                <input type="text" placeholder="Search to..."></input>
+                <input
+                  value={value}
+                  onChange={(evt) => setValue(evt.target.value)}
+                  onKeyDown={(evt) => enter(evt)}
+                  type="text"
+                  placeholder="Search to..."
+                ></input>
 
-                <button className="btn">
+                <button className="btn" onClick={searchRepositores}>
                   <svg
                     width="20"
                     height="20"
@@ -51,76 +80,16 @@ export default function Home() {
               </div>
 
               <div className="card">
-                <div className="card-body">
-                  <h2>600,228 repository results</h2>
-
-                  <ul className="repositories">
-                    {repositories.map((repo) => (
-                      <li key={repo.id} className="repository-item">
-                        <Link
-                          to={"/view/" + repo.owner.login + "/" + repo.name}
-                          className="repository-link"
-                        >
-                          <svg
-                            width="12"
-                            height="16"
-                            viewBox="0 0 12 16"
-                            version="1.1"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              className="fill"
-                              fillRule="evenodd"
-                              d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
-                            ></path>
-                          </svg>
-
-                          <div className="repository-content">
-                            <h3 className="repository-title">
-                              {repo.owner.login}/<span>{repo.name}</span>
-                            </h3>
-
-                            {repo.description && (
-                              <p className="repository-description">
-                                {repo.description}
-                              </p>
-                            )}
-
-                            <ul className="repository-info">
-                              <li className="stargazers">
-                                <svg
-                                  width="14"
-                                  height="16"
-                                  viewBox="0 0 14 16"
-                                  version="1.1"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                >
-                                  <path
-                                    className="fill"
-                                    fillRule="evenodd"
-                                    d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
-                                  ></path>
-                                </svg>
-
-                                {repo.stargazers_count}
-                              </li>
-
-                              <li className="language">
-                                <div className="language-color"></div>
-
-                                {repo.language}
-                              </li>
-
-                              <li className="updated">
-                                Updated at {repo.updated_at}
-                              </li>
-                            </ul>
-                          </div>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+                {repositories.total_count > 0 ? (
+                  <div className="card-body">
+                    <h2>{repositories.total_count} repository results</h2>
+                    <RepositoriesList repositories={repositories.items} />
+                  </div>
+                ) : (
+                  <div className="card-body">
+                    <p className="empty">No entry found</p>
+                  </div>
+                )}
               </div>
             </main>
           </div>
--- src/components/RepositoryDetail.js
+++ src/components/RepositoryDetail.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect, useCallback } from "react";
+import RepositoriesList from "./RepositoriesList";
 import "../scss/RepositoryDetail.scss";
 
-const API_URL = "https://api.github.com/repos";
+const API_URL = "https://api.github.com";
 
 export default function RepositoryDetail({ match }) {
   const [repository, setRepository] = useState([]);
+  const [repositoriesFromAuthor, setRepositoriesFromAuthor] = useState([]);
 
   const fetchData = useCallback(async () => {
     const response = await fetch(
-      API_URL + "/" + match.params.owner + "/" + match.params.repo
+      API_URL + "/repos/" + match.params.owner + "/" + match.params.repo
     );
     const data = await response.json();
-    console.log(data);
     setRepository(data);
   }, [match.params.owner, match.params.repo]);
 
+  const fetchRepositoriesFromAuthor = useCallback(async () => {
+    const response = await fetch(
+      API_URL + "/users/" + match.params.owner + "/repos"
+    );
+    const data = await response.json();
+
+    console.log(data);
+    setRepositoriesFromAuthor(data);
+  }, [match.params.owner]);
+
   useEffect(() => {
     fetchData();
-  }, [fetchData]);
+    fetchRepositoriesFromAuthor();
+  }, [fetchData, fetchRepositoriesFromAuthor]);
 
   return (
     <div className="repository-detail">
@@ -35,8 +47,110 @@ export default function RepositoryDetail({ match }) {
                     <p className="description">{repository.description}</p>
                   )}
 
-                  <a href={repository.html_url} className="btn">
+                  <div className="info">
+                    <div className="info-row">
+                      <div className="info-col">
+                        <svg
+                          width="14"
+                          height="16"
+                          viewBox="0 0 14 16"
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            className="fill"
+                            fillRule="evenodd"
+                            d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 011.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"
+                          ></path>
+                        </svg>
+
+                        <span>0 issues</span>
+                      </div>
+
+                      <div className="info-col">
+                        <svg
+                          width="12"
+                          height="16"
+                          viewBox="0 0 12 16"
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            className="fill"
+                            fillRule="evenodd"
+                            d="M11 11.28V5c-.03-.78-.34-1.47-.94-2.06C9.46 2.35 8.78 2.03 8 2H7V0L4 3l3 3V4h1c.27.02.48.11.69.31.21.2.3.42.31.69v6.28A1.993 1.993 0 0010 15a1.993 1.993 0 001-3.72zm-1 2.92c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zM4 3c0-1.11-.89-2-2-2a1.993 1.993 0 00-1 3.72v6.56A1.993 1.993 0 002 15a1.993 1.993 0 001-3.72V4.72c.59-.34 1-.98 1-1.72zm-.8 10c0 .66-.55 1.2-1.2 1.2-.65 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
+                          ></path>
+                        </svg>
+
+                        <span>0 Pull requests</span>
+                      </div>
+                    </div>
+
+                    <div className="info-row">
+                      <div className="info-col">
+                        <svg
+                          width="14"
+                          height="16"
+                          viewBox="0 0 14 16"
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            className="fill"
+                            fillRule="evenodd"
+                            d="M8 13H6V6h5v2H8v5zM7 1C4.81 1 2.87 2.02 1.59 3.59L0 2v4h4L2.5 4.5C3.55 3.17 5.17 2.3 7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 011.3 8c0-.34.03-.67.09-1H.08C.03 7.33 0 7.66 0 8c0 3.86 3.14 7 7 7s7-3.14 7-7-3.14-7-7-7z"
+                          ></path>
+                        </svg>
+
+                        <span>3 commits</span>
+                      </div>
+
+                      <div className="info-col">
+                        <svg
+                          width="10"
+                          height="16"
+                          viewBox="0 0 10 16"
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            className="fill"
+                            fillRule="evenodd"
+                            d="M10 5c0-1.11-.89-2-2-2a1.993 1.993 0 00-1 3.72v.3c-.02.52-.23.98-.63 1.38-.4.4-.86.61-1.38.63-.83.02-1.48.16-2 .45V4.72a1.993 1.993 0 00-1-3.72C.88 1 0 1.89 0 3a2 2 0 001 1.72v6.56c-.59.35-1 .99-1 1.72 0 1.11.89 2 2 2 1.11 0 2-.89 2-2 0-.53-.2-1-.53-1.36.09-.06.48-.41.59-.47.25-.11.56-.17.94-.17 1.05-.05 1.95-.45 2.75-1.25S8.95 7.77 9 6.73h-.02C9.59 6.37 10 5.73 10 5zM2 1.8c.66 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2C1.35 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2zm0 12.41c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm6-8c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
+                          ></path>
+                        </svg>
+
+                        <span>1 branch</span>
+                      </div>
+                    </div>
+
+                    <div className="info-row">
+                      <div className="info-col">
+                        <div className="language-color"></div>
+
+                        <span>TypeScript</span>
+                      </div>
+
+                      <div className="info-col">Created at 4 hours ago</div>
+                    </div>
+                  </div>
+
+                  <a
+                    href={repository.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn"
+                  >
                     View on Github
+                  </a>
+
+                  <a
+                    href={repository.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn"
+                  >
+                    Author's page
                   </a>
                 </div>
               </div>
@@ -44,7 +158,15 @@ export default function RepositoryDetail({ match }) {
               <div className="card card-from-author">
                 <div className="card-header">From author</div>
 
-                <div className="card-body"></div>
+                {repositoriesFromAuthor.length ? (
+                  <div className="card-body">
+                    <RepositoriesList repositories={repositoriesFromAuthor} />
+                  </div>
+                ) : (
+                  <div className="card-body">
+                    <p className="empty">O author não possui repositories;</p>
+                  </div>
+                )}
               </div>
             </main>
           </div>
